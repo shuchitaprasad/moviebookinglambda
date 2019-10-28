b0afb5e4afa5b812d416df5cd99886a37ca8f9a9
Shuchita Prasad <shuchitaprasad@gmail.com>
2019-10-28 15:52:35 +0530

    Create movielist.py

---
+++ movielist.py
@@ -0,0 +1,39 @@
+
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
+def fetchMovieList():
+
+    return  [
+  {
+    'name': 'Mangal Mission',
+    'slotDetails': [
+      {
+        'slots': '10 -12',
+        'price': '500'
+      },
+      {
+        'slots': '12 -2',
+        'price': '700'
+      }
+    ]
+  },
+  {
+    'name': 'WAR',
+    'slotDetails': [
+      {
+        'slots': '10 -12',
+        'price': '400'
+      },
+      {
+        'slots': '12 -2',
+        'price': '600'
+      }
+    ]
+  }
+]
+
+
+
